f3b099ede3ddff1033ade1d2dad20e2efebc77f8
Aaron Sum <sumsx03@gmail.com>
2018-02-19 10:25:30 +0100

    First version of the program.

---
+++ totalSum.java
@@ -0,0 +1,15 @@
+public class totalSum {
+    /**
+    *   Calculate the sum of number 1 to n.
+    *   @param integer number n.
+    *   @return integer total sum of number.
+    */
+    private static int totalSumOfNumber (final int number) {
+        int totalSum = number * (number + 1) / 2;
+        return  totalSum;
+    }
+
+    public static void main(String[] args) {
+        System.out.format("The total sum from 1 to 100 is %d.\n", totalSumOfNumber(100));
+    }
+}
