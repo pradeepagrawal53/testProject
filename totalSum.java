872179645cca0a5e6c6c1ab707427b065ffc636b
Aaron Sum <sumsx03@gmail.com>
2018-02-19 10:39:06 +0100

    Added code to time the program.

--- totalSum.java
+++ totalSum.java
@@ -10,6 +10,11 @@ public class totalSum {
     }
 
     public static void main(String[] args) {
+        long timeStart = System.nanoTime();
         System.out.format("The total sum from 1 to 100 is %d.\n", totalSumOfNumber(100));
+        long timeEnd = System.nanoTime();
+        long timeTaken = timeStart - timeEnd;
+        System.out.format("Time taken to run the program is %d.\n", timeTaken);
+
     }
 }
